ed72d5d83e8a575011c5808d2d2717bdce7b93c3
Dipanshu Batra <devbatra887@gmail.com>
2026-04-03 10:19:39 +0530

    clean services

---
+++ product_service/src/main/java/com/dipanshu/product_service/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.dipanshu.product_service.repository;
+
+import com.dipanshu.product_service.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+}
